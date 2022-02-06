60449cf7f5982f53dbd94fcce0d93e31c764da43
back hae ran <idg9833@gmail.com>
2022-02-06 17:06:40 +0900

    notice 앱 생성/ urls 및 setting 등록/ models 추가/ admin 추가/migrate 생성

--- redanse/urls.py
+++ redanse/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('notice/', include('notice.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
