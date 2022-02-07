b3ff22a47fec419664018d225382603a88764761
back hae ran <idg9833@gmail.com>
2022-02-07 22:25:03 +0900

    cart 앱/ models admin migrate 생성

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from user.models import User
+from clothes.models import Clothes
 
-# Create your models here.
+
+class Cart(models.Model):
+    cart_num = models.AutoField(primary_key=True)
+    payment_status = models.IntegerField(choices=[(0, 0), (1, 1)])
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    clothes_num = models.ForeignKey(Clothes, on_delete=models.CASCADE, db_column='clothes_num')
+
+    def __int__(self) -> int:
+        return self.cart_num
+
+    class Meta:
+        ordering = ["-cart_num"]
+
+
