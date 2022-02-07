f6586f4f506f7456023bf47d8e71ec0f65429fda
back hae ran <idg9833@gmail.com>
2022-02-07 22:28:38 +0900

    community 앱/ models admin migrate 생성

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from community.models import Community
 
-# Register your models here.
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    pass
--- community/models.py
+++ community/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Community(models.Model):
+    community_num = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    img1 = models.ImageField(upload_to="redanse/community/%Y/%m/%d/%H/%M/%S", blank=True)
+    img2 = models.ImageField(upload_to="redanse/community/%Y/%m/%d/%H/%M/%S", blank=True)
+    img3 = models.ImageField(upload_to="redanse/community/%Y/%m/%d/%H/%M/%S", blank=True)
+    img4 = models.ImageField(upload_to="redanse/community/%Y/%m/%d/%H/%M/%S", blank=True)
+    img5 = models.ImageField(upload_to="redanse/community/%Y/%m/%d/%H/%M/%S", blank=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-community_num"]
+
+
+
+
