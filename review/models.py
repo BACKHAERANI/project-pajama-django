ca631600f22bc17bb9581ef97cd41a71b0f65f9e
Hyeonil Na <sjrnfl528@gmail.com>
2022-02-08 09:58:03 +0900

    review 모델추가

--- review/models.py
+++ review/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
+from payment.models import Payment_detail
 
-# Create your models here.
+
+class Review(models.Model):
+    payment_detail_num = models.ForeignKey(Payment_detail)
+    score = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(10),
+        ])
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    img1 = models.ImageField(upload_to="redanse/review/%Y/%m/%d/%H/%M/%S", blank=True)
+    img2 = models.ImageField(upload_to="redanse/review/%Y/%m/%d/%H/%M/%S", blank=True)
+    img3 = models.ImageField(upload_to="redanse/review/%Y/%m/%d/%H/%M/%S", blank=True)
+    img4 = models.ImageField(upload_to="redanse/review/%Y/%m/%d/%H/%M/%S", blank=True)
+    img5 = models.ImageField(upload_to="redanse/review/%Y/%m/%d/%H/%M/%S", blank=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-clothes_num"]
---
+++ review/urls.py
@@ -0,0 +1,3 @@
+app_name = "review"
+
+urlpatterns = []
