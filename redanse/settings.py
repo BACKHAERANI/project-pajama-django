15b1a65aea54e544be39c90b616862b9dd8ceeba
back hae ran <idg9833@gmail.com>
2022-02-09 15:31:48 +0900

    (수정)settings / CORS_ALLOWED_ORIGINS 추가 / MIDDLEWARE corsheaders 설정 추가

--- redanse/settings.py
+++ redanse/settings.py
@@ -61,6 +61,7 @@ if DEBUG:
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,6 +153,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
