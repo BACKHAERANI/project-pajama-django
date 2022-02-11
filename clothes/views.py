78c6f151d6a9c2cb5a06ae4c03fc087cd41f4f72
Hyeonil Na <sjrnfl528@gmail.com>
2022-02-11 16:32:52 +0900

    clothes앱 / 검색 설정

--- clothes/views.py
+++ clothes/views.py
@@ -8,3 +8,12 @@ from clothes.serializers import ClothesSerializer
 class ClothesViewSet(ModelViewSet):
     queryset = Clothes.objects.all()
     serializer_class = ClothesSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.query_params.get("query", "")
+        if query:
+            qs = qs.filter(title__icontains=query)
+
+        return qs
