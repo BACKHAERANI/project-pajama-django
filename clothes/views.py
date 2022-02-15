fcfff98f8d66f2df31f1451c915b2e9f4c37858e
Hyeonil Na <sjrnfl528@gmail.com>
2022-02-15 10:32:48 +0900

    clothes/views 페이지처리 추가

--- clothes/views.py
+++ clothes/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from clothes.models import Clothes
 from clothes.serializers import ClothesSerializer
 
 
+class ClothesPagination(PageNumberPagination):
+    page_size = 9
+    page_size_query_param = 'page_size'
+    max_page_size = 1
+
+
 class ClothesViewSet(ModelViewSet):
     queryset = Clothes.objects.all()
     serializer_class = ClothesSerializer
+    pagination_class = ClothesPagination
 
     def get_queryset(self):
         qs = super().get_queryset()
