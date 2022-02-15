9982214a2513bd4dd453b8893c51659ec7642aaf
Yerin Park <1472moment@gmail.com>
2022-02-15 16:56:55 +0900

    QnA user_id 표출//qna/serializers.py qna/views.py

--- qna/views.py
+++ qna/views.py
@@ -2,13 +2,17 @@ import json
 from django.http import HttpResponse
 from rest_framework import viewsets
 from qna.models import Qna
-from qna.serializers import QnaSerializer
+from qna.serializers import QnaSerializer, QnaCreateSerializer
 
 
 class QnaViewSet(viewsets.ModelViewSet):
     queryset = Qna.objects.all()
-    serializer_class = QnaSerializer
-
+    def get_serializer_class(self):
+        method = self.request.method
+        if method == 'PUT' or method == 'POST':
+            return QnaCreateSerializer
+        else:
+            return QnaSerializer
 
 def qna_list(request):
     qs = Qna.objects.all()
