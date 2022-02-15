46afa5e0640ce758050fd15753f1093aa6752d84
back hae ran <idg9833@gmail.com>
2022-02-15 09:56:19 +0900

    qna 앱 // serializers put, post 분리하고 urls, view 수정

--- qna/views.py
+++ qna/views.py
@@ -1,28 +1,18 @@
-import json
-from django.http import HttpResponse
 from rest_framework import viewsets
 from qna.models import Qna
-from qna.serializers import QnaSerializer
+from qna.serializers import QnaSerializer, QnaCreateSerializer
 
 
 class QnaViewSet(viewsets.ModelViewSet):
     queryset = Qna.objects.all()
     serializer_class = QnaSerializer
 
+    def get_serializer_class(self):
+        method = self.request.method
+        if method == 'PUT' or method == 'POST':
+            return QnaCreateSerializer
+        else:
+            return QnaSerializer
 
-def qna_list(request):
-    qs = Qna.objects.all()
-    data = [
-        {
-            "qna_num": qna.qna_num,
-            "title": qna.title,
-            "content": qna.content,
-            "answer": qna.answer,
-            "img": qna.img,
-            "registration_date": qna.registration_date,
-            "user_id": qna.user_id,
-        }
-        for qna in qs
-    ]
-    json_string = json.dumps(data)
-    return HttpResponse(json_string)
+
+
