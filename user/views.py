55e345da6872a1f787b2650d2ed49a2cd4a0c166
back hae ran <idg9833@gmail.com>
2022-03-03 18:53:37 +0900

    user 앱 // 페이징 all 추가

--- user/views.py
+++ user/views.py
@@ -17,6 +17,11 @@ class UserPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 1
 
+    def paginate_queryset(self, queryset, request, view=None):
+        if "all" in request.query_params:
+            return None
+        return super().paginate_queryset(queryset, request, view)
+
 
 class UserAPIView(CreateAPIView):
     queryset = User.objects.all()
